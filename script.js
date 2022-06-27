2772c0d70a3ad543e450e0b9c31877f92f837096
Leonardo Passarini <leogpassarini@gmail.com>
2022-06-27 16:12:41 -0300

    preload e renderizacao de img

---
+++ script.js
@@ -0,0 +1,7 @@
+const loadingBox = document.querySelector(".loading-box");
+const site = document.querySelector(".site");
+
+function loading() {
+  loadingBox.style.display = "none";
+  site.style.display = "block";
+}
